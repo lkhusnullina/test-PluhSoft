569a19c4a68f8e7036e08031282ffce39d5d3221
Любовь Хуснуллина <lyuba140296@gmail.com>
2024-11-07 00:58:25 +0500

    диаграмма процесса

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Project from '../components/Project';
+import { useNavigate } from 'react-router-dom';
 
 function Projects() {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const goToForm = () => {
+    navigate("/application-form"); 
+  };
   return(
     <div className="page-content page-content--projects">
       <h1 className="page-content__title">{t('projects')}</h1>
       <Project/>
       <Project/>
       <Project/>
+      <button className="page-content__application" onClick={goToForm}>{t("homeContentForm")}</button>
     </div>
   );
 }
